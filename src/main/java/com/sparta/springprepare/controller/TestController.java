41d5c8eb116d5730455dd19761e0ba462d247fb8
강언덕 <64720058+123456789qwaszx@users.noreply.github.com>
2026-08-28 17:23:57 +0900

    Query Parameter 실습

--- src/main/java/com/sparta/springprepare/controller/TestController.java
+++ src/main/java/com/sparta/springprepare/controller/TestController.java
@@ -2,6 +2,7 @@ package com.sparta.springprepare.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,13 @@ public class TestController {
     @GetMapping("/hello/{name}/{age}")
     public String helloName(@PathVariable String name, @PathVariable int age) {
         return "Hello " + name + " " + age;
+    }
+
+    @GetMapping("/hello/age")
+    public String helloAge(
+            @RequestParam String name,
+            @RequestParam(required = false, defaultValue = "0") int age
+    ) {
+        return name + "은 " + age + "살입니다.";
     }
 }
